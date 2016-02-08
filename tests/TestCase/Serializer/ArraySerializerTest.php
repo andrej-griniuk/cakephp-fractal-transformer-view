4c2ff92515b999c449ee13a5d9a51c9068b26378
Andrej Griniuk <andrej.griniuk@gmail.com>
2016-02-09 02:02:39 +1100

    ArraySerializerTest

---
+++ tests/TestCase/Serializer/ArraySerializerTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace FractalTransformerView\Test\TestCase\Serializer;
+
+use Cake\Core\Configure;
+use Cake\TestSuite\TestCase;
+use FractalTransformerView\Serializer\ArraySerializer;
+
+/**
+ * ArraySerializerTest
+ */
+class ArraySerializerTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        Configure::write('debug', false);
+    }
+
+    public function testCollection()
+    {
+        $serializer = new ArraySerializer();
+        $data = ['test'];
+
+        $this->assertEquals($data, $serializer->collection('data', $data));
+    }
+}
